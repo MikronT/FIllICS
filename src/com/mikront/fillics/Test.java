6e795d72b4d1c5bada765de8c790ce5728e2e48a
MikronT <vlad.ivanov.if@gmail.com>
2022-09-25 01:05:26 +0300

    Update Test.java

--- src/com/mikront/fillics/Test.java
+++ src/com/mikront/fillics/Test.java
@@ -2,11 +2,13 @@ package com.mikront.fillics;
 
 import com.mikront.fillics.schedule.Day;
 import com.mikront.fillics.schedule.Parser;
-import com.mikront.fillics.schedule.Request;
 import com.mikront.util.Log;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 
@@ -14,19 +16,16 @@ public class Test {
     private static final File FILE_SCHEDULE = new File("schedule.html");
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Log.ging(true);
         Log.level(Log.LEVEL_VERBOSE);
 
-        Document document = Request.schedule(null, "ІП-20-3", null, null);
-        //Document document = Jsoup.parse(FILE_SCHEDULE, StandardCharsets.UTF_8.name());
+        Document document = Jsoup.parse(FILE_SCHEDULE, StandardCharsets.UTF_8.name());
         List<Day> days = Parser.of(document)
                 .setDefaultGroup(Main.DEFAULT_GROUP)
                 .parse();
 
-        Log.d("Test::main: days = [");
         for (Day day : days)
-            Log.d("Test::main:     " + day);
-        Log.d("Test::main: ]");
+            Log.d(day);
     }
 }
