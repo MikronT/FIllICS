eb28ef7ae7567aa858534b4bdf4dd590d6c913f0
MikronT <vlad.ivanov.if@gmail.com>
2022-09-25 01:04:30 +0300

    Add subject name and type mapping

--- src/com/mikront/fillics/gui/MainForm.java
+++ src/com/mikront/fillics/gui/MainForm.java
@@ -10,21 +10,28 @@ import org.jsoup.nodes.Document;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class MainForm extends Form {
     private static final File FILE_ICS = new File("import.ics");
+    private static final File FILE_MAP_SUBJECTS = new File("map_subjects.txt");
+    private static final File FILE_MAP_TYPES = new File("map_types.txt");
     private static final String ITEM_UNSET = "";
     private static final int PROGRESS_MAX = 100;
 
     private JComboBox<String> combo_groups, combo_teachers;
     private JProgressBar progressBar;
     private XSpinnerDateModel model_from, model_to;
+    private final HashMap<String, String> map_subjects = new HashMap<>();
+    private final HashMap<String, String> map_types = new HashMap<>();
     private final JPanel container = getContainer();
 
 
@@ -53,16 +60,15 @@ public class MainForm extends Form {
         var label_to = new JLabel(LABEL_DATE_TO);
 
         combo_teachers = newJComboBox();
-        combo_groups = newJComboBox();
-
         combo_teachers.addItemListener(e -> combo_groups.setSelectedItem(ITEM_UNSET));
+
+        combo_groups = newJComboBox();
         combo_groups.addItemListener(e -> combo_teachers.setSelectedItem(ITEM_UNSET));
 
         model_from = new XSpinnerDateModel();
         model_to = new XSpinnerDateModel();
-        var spinner_from = newJSpinner(model_from);
-        var spinner_to = newJSpinner(model_to);
 
+        var spinner_from = newJSpinner(model_from);
         spinner_from.addChangeListener(e -> {
             LocalDate
                     date1 = model_from.getDate(),
@@ -70,6 +76,7 @@ public class MainForm extends Form {
             if (date1.compareTo(date2) > 0)
                 model_to.setValue(date1);
         });
+        var spinner_to = newJSpinner(model_to);
         spinner_to.addChangeListener(e -> {
             LocalDate
                     date1 = model_from.getDate(),
@@ -167,14 +174,45 @@ public class MainForm extends Form {
         setProgress(50, STEP_GETTING_TEACHERS);
 
         combo_teachers.setEnabled(false);
+        combo_groups.setEnabled(false);
+
         Request.teachers().forEach(combo_teachers::addItem);
         combo_teachers.setEnabled(true);
 
         setProgress(PROGRESS_MAX, STEP_GETTING_GROUPS);
 
-        combo_groups.setEnabled(false);
         Request.groups().forEach(combo_groups::addItem);
         combo_groups.setEnabled(true);
+
+        try (var stream = new FileInputStream(FILE_MAP_SUBJECTS);
+             var scanner = new Scanner(stream)) {
+            map_subjects.clear();
+            while (scanner.hasNextLine()) {
+                var strings = scanner.nextLine().split(";");
+                if (strings.length != 2)
+                    continue;
+                map_subjects.put(strings[0], strings[1]);
+            }
+        } catch (IOException e) {
+            Log.w("MainForm::requestLists: unable to load subjects map");
+            Log.w("MainForm::requestLists:   - file = " + FILE_MAP_SUBJECTS);
+            Log.w("MainForm::requestLists:   = catching: ", e);
+        }
+
+        try (var stream = new FileInputStream(FILE_MAP_TYPES);
+             var scanner = new Scanner(stream)) {
+            map_types.clear();
+            while (scanner.hasNextLine()) {
+                var strings = scanner.nextLine().split(";");
+                if (strings.length != 2)
+                    continue;
+                map_types.put(strings[0], strings[1]);
+            }
+        } catch (IOException e) {
+            Log.w("MainForm::requestLists: unable to load types map");
+            Log.w("MainForm::requestLists:   - file = " + FILE_MAP_TYPES);
+            Log.w("MainForm::requestLists:   = catching: ", e);
+        }
 
         resetProgress();
     }
@@ -204,17 +242,24 @@ public class MainForm extends Form {
                 model_from.getDate(),
                 model_to.getDate());
 
+        setProgress(100, STEP_COMPILING_DATA);
+
         List<Day> days = Parser.of(doc)
                 .setDefaultGroup(group)
                 .parse();
 
-        setProgress(100, STEP_COMPILING_DATA);
-
         CalendarData data = new CalendarData();
         for (Day day : days)
             for (Cell cell : day)
-                for (Session session : cell)
+                for (Session session : cell) {
+                    String subject = session.getSubject();
+                    String type = session.getType();
+
+                    if (map_subjects.containsKey(subject)) session.setSubject(map_subjects.get(subject));
+                    if (map_types.containsKey(type)) session.setType(map_types.get(type));
+
                     data.addEvent(session.toEvent(day, cell));
+                }
 
         try (var stream = new FileOutputStream(FILE_ICS)) {
             stream.write(data.compile().getBytes(StandardCharsets.UTF_8));
