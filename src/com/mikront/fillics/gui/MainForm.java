6817d0ec4fd543a6f5d8110d352930c9b3cfcc54
MikronT <vlad.ivanov.if@gmail.com>
2022-09-24 11:01:08 +0300

    Add basic UI components

--- src/com/mikront/fillics/gui/MainForm.java
+++ src/com/mikront/fillics/gui/MainForm.java
@@ -1,8 +1,10 @@
 package com.mikront.fillics.gui;
 
+import com.mikront.fillics.schedule.Request;
 import com.mikront.util.Log;
 
 import javax.swing.*;
+import java.awt.*;
 
 
 public class MainForm extends Form {
@@ -19,6 +21,38 @@ public class MainForm extends Form {
     @Override
     protected void onCreate() {
         super.onCreate();
+
+        var frame = getFrame();
+        frame.setMinimumSize(new Dimension(300, 240));
+
+        var layout = new GroupLayout(container);
+        container.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+
+        var label_teacher = new JLabel(LABEL_TEACHER);
+
+        var comboBox_teachers = new JComboBox<String>();
+        Request.teachers().forEach(comboBox_teachers::addItem);
+
+        var label_group = new JLabel(LABEL_GROUP);
+
+        var comboBox_groups = new JComboBox<String>();
+        Request.groups().forEach(comboBox_groups::addItem);
+
+
+        var label_date_from = new JLabel(LABEL_DATE_FROM);
+
+        var spinner_date_from = new JSpinner();
+
+        var label_date_to = new JLabel(LABEL_DATE_TO);
+
+        var spinner_date_to = new JSpinner();
+
+
+        var button_request = new JButton(BUTTON_REQUEST);
+
 
         Components.applyDefaults(container);
     }
