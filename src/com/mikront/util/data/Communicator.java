f4063cf9ef40c626650c7224ff883e33c24aa925
MikronT <vlad.ivanov.if@gmail.com>
2023-02-21 01:21:11 +0200

    Add test Communicator and LiveData implementation

---
+++ src/com/mikront/util/data/Communicator.java
@@ -0,0 +1,9 @@
+package com.mikront.util.data;
+
+/**
+ * Abstract class to extend to communicate between different places in the program and keep the data synchronized
+ * using {@link LiveData}
+ */
+public abstract class Communicator {
+    public Communicator() {}
+}
