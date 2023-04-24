ccc0a449769b4d2eeda9f3bcf56e06895021a754
MikronT <vlad.ivanov.if@gmail.com>
2023-04-24 20:53:29 +0300

    Add automatic requested page caching

--- src/com/mikront/fillics/schedule/Schedule.java
+++ src/com/mikront/fillics/schedule/Schedule.java
@@ -1,14 +1,18 @@
 package com.mikront.fillics.schedule;
 
+import com.mikront.util.Concat;
 import com.mikront.util.Utils;
 import com.mikront.util.debug.Log;
 import org.jetbrains.annotations.Nullable;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.NodeFilter;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -19,6 +23,7 @@ public class Schedule {
     public static final File CACHE_DIR = new File("cache");
     public static final LocalDate DATE_TO = LocalDate.now().withMonth(8).withDayOfMonth(1);
     public static final LocalDate DATE_FROM = DATE_TO.minusYears(1);
+    private static final Charset CHARSET = Charset.forName("windows-1251");
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final String URL = "https://dekanat.nung.edu.ua/cgi-bin/timetable.cgi";
 
@@ -74,17 +79,59 @@ public class Schedule {
                 .data("sdate", DATE_FROM.format(FORMATTER))
                 .data("edate", DATE_TO.format(FORMATTER));
 
-        if (Utils.notEmpty(teacher)) connection = connection.data("teacher", teacher);
-        if (Utils.notEmpty(group)) connection = connection.data("group", group);
+        String criteria = null;
+        if (Utils.notEmpty(teacher)) {
+            connection = connection.data("teacher", teacher);
+            criteria = teacher;
+        } else if (Utils.notEmpty(group)) {
+            connection = connection.data("group", group);
+            criteria = group;
+        }
 
+        Document doc;
         try {
-            return connection
-                    .postDataCharset("windows-1251")
+            doc = connection
+                    .postDataCharset(CHARSET.name())
                     .post();
         } catch (IOException e) {
             Log.e("Schedule::getSchedule: unable to get schedule");
             Log.e("Schedule::getSchedule:   = catching: ", e);
+            return null;
         }
-        return null;
+
+        //Try saving requested page for future offline use
+        var cacheFile = new File(Concat.me()
+                .word(CACHE_DIR)
+                .word(File.separatorChar)
+                .word(DATE_FROM.getYear()).word("-").word(DATE_TO.getYear())
+                .word("_")
+                .when(Utils.notEmpty(criteria))
+                .word(criteria)
+                .word("_")
+                .then()
+                .word(LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE))
+                .word(".html")
+                .enate());
+
+        if (!CACHE_DIR.exists() && !CACHE_DIR.mkdirs()) {
+            Log.w("Schedule::getSchedule: can't create cache directory");
+            Log.w("Schedule::getSchedule:   - CACHE_DIR = " + CACHE_DIR.getAbsolutePath());
+        }
+
+        try (var stream = new FileOutputStream(cacheFile)) {
+            stream.write(doc
+                    .filter((node, depth) ->
+                            node.toString().contains("charset") ?
+                                    NodeFilter.FilterResult.REMOVE :
+                                    NodeFilter.FilterResult.CONTINUE)
+                    .outerHtml()
+                    .getBytes(CHARSET));
+        } catch (IOException e) {
+            Log.w("Schedule::getSchedule: unable to save cache file");
+            Log.w("Schedule::getSchedule:   - cacheFile = " + cacheFile);
+            Log.w("Schedule::getSchedule:   = catching: ", e);
+        }
+
+        return doc;
     }
 }
