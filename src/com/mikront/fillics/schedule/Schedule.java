ec0b632f97f02e65a773d9967df669f2ee5404c3
MikronT <vlad.ivanov.if@gmail.com>
2023-09-04 22:46:55 +0300

    Fix date range filter

--- src/com/mikront/fillics/schedule/Schedule.java
+++ src/com/mikront/fillics/schedule/Schedule.java
@@ -21,8 +21,8 @@ import java.util.List;
 
 public class Schedule {
     public static final File CACHE_DIR = new File("cache");
-    public static final LocalDate DATE_TO = LocalDate.now().withMonth(8).withDayOfMonth(1);
-    public static final LocalDate DATE_FROM = DATE_TO.minusYears(1);
+    public static final LocalDate DATE_FROM = LocalDate.now().withMonth(8).withDayOfMonth(1);
+    public static final LocalDate DATE_TO = DATE_FROM.plusYears(1);
     private static final Charset CHARSET = Charset.forName("windows-1251");
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     private static final String URL = "https://dekanat.nung.edu.ua/cgi-bin/timetable.cgi";
