4dac4a9b84bf2b8110dfa51d68d8af457b1e294b
MikronT <vlad.ivanov.if@gmail.com>
2024-09-10 14:00:41 +0300

    Remove redundant live data implementation

--- src/com/mikront/util/data/Communicator.java
+++
@@ -1,9 +0,0 @@
-package com.mikront.util.data;
-
-/**
- * Abstract class to extend to communicate between different places in the program and keep the data synchronized
- * using {@link LiveData}
- */
-public abstract class Communicator {
-    public Communicator() {}
-}
